2ede655d80fc622ed018a157e47570bebc7b5b53
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-06-04 13:03:17 +0100

    add no tls and ssl for next auth

--- task-management/pages/api/auth/[...nextauth].ts
+++ task-management/pages/api/auth/[...nextauth].ts
@@ -19,10 +19,14 @@ export default NextAuth({
       server: {
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
+        secure: false,
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASSWORD,
         },
+        tls: {
+          rejectUnauthorized: false
+        }
       },
       from: process.env.SMTP_FROM
     })
