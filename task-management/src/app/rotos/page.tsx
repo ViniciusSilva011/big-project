69c48954e28f21d09d05f28b673837ff9207e5b8
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-06-07 20:50:58 +0100

    fix: change to app src

---
+++ task-management/src/app/rotos/page.tsx
@@ -0,0 +1,9 @@
+const teste = () => {
+  return (
+    <div className=' grid bg-red-400 h-screen place-items-center'>
+      <h1 className='text-lg'>rotos page</h1>
+    </div>
+  );
+};
+
+export default teste;
