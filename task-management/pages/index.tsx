3989af7b739938f8eb124250094ef05646da90b8
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-06-09 23:31:36 +0100

    style: login page

--- task-management/pages/index.tsx
+++ task-management/pages/index.tsx
@@ -3,6 +3,8 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import './globals.css';
 import Header from '@/app/components/Header';
 import Loading from './Loading';
+import NavBar from '@/app/components/NavBar';
+import Login from './auth/Login';
 
 const IndexPage = () => {
   const { status, data: session } = useSession();
@@ -14,15 +16,26 @@ const IndexPage = () => {
     const userLoggedName = session?.user?.name ?? '';
     return (
       <div className='m-3'>
-        <Header user={userLoggedName} />
-        <main className='flex min-h-screen items-center justify-center'></main>
+        <NavBar user={userLoggedName} />
+        <main className='min-h-screen mt-5'>
+          <header className='bg-white shadow'>
+            <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+              <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+                Dashboard
+              </h1>
+            </div>
+          </header>
+          <main>
+            <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'></div>
+          </main>
+        </main>
       </div>
     );
   } else {
     return (
       <div>
-        You are not logged in! <br />
         <button onClick={() => signIn()}>Sign in</button>
+        <Login />
       </div>
     );
   }
