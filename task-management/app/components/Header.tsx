4931ae7c4159d72967fd7981d92e2ea4ab6c8004
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-06-09 23:31:51 +0100

    feature: fix nav

--- task-management/app/components/Header.tsx
+++ task-management/app/components/Header.tsx
@@ -1,33 +1,9 @@
 'use client';
-import Image from 'next/image';
+
 import NavBar from './NavBar';
-import { signOut } from 'next-auth/react';
 
 const Header = ({ user = '' }) => {
-  return (
-    <div className='flex items-center justify-between '>
-      <NavBar />
-
-      <div className=''>
-        <div className='flex items-center justify-items-center'>
-          <div className='block'>
-            <p>{user}</p>
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-
-          <Image
-            src={
-              'https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg'
-            }
-            width='384'
-            height='512'
-            alt={''}
-            className='w-12 h-12 rounded-full mx-auto ml-2'
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <NavBar user={user} />;
 };
 
 export default Header;
