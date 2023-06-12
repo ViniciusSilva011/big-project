6ce667822b08fb4c1e7c6e3fef137b578994e693
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-06-12 19:20:39 +0100

    add middleware file, add next-auth tweaks

---
+++ task-management/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware"
+import { getServerSession } from "next-auth";
+
+
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req,res) {
+      console.log(req.nextauth.token)
+    },
+    {
+      callbacks: {
+         authorized: async (s) => {
+            console.log('res :>> ', s.req.cookies.get('next-auth.session-token'));
+            return true;
+        },
+      },
+    }
+  )
+export const config = {
+    matcher: '/api/:path*',
+}
--- task-management/pages/api/auth/[...nextauth].ts
+++ task-management/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Email from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials"
 
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
@@ -18,7 +19,41 @@ const smtpPassword = process.env.SMTP_PASSWORD!;
 const smtpFrom = process.env.SMTP_FROM!;
 
 export default NextAuth({
+  session: { strategy: "database" },
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const res = await fetch("/auth/login", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const user = await res.json()
+  
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    }),
     GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -39,6 +74,16 @@ export default NextAuth({
       from: smtpFrom
     })
   ],
+  callbacks: {
+     async jwt({token,account,profile}) {
+      console.log('token, account,profile :>> ', token, account,profile);
+      if (account) {
+        token.accessToken = account.access_token
+        token.name = profile?.name
+      }
+      return token;
+    }
+  },
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET!,
 });
