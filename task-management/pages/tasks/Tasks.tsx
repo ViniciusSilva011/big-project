aa2d81c7f2a44258290c8ecbbdd094194ba58165
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-06-18 00:13:40 +0100

    style: change layout

--- task-management/pages/tasks/Tasks.tsx
+++ task-management/pages/tasks/Tasks.tsx
@@ -7,7 +7,7 @@ export default function Tasks() {
     <>
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Task List</h1>
-        <ul className="bg-slate-950 divide-y divide-gray-900">
+        <ul className="bg-slate-800 divide-y divide-gray-900">
           {isLoading && <Loading />}
           {tasks &&
             tasks.map((task: any) => {
