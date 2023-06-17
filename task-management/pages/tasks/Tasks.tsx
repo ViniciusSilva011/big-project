caae0c7f44014fbc1bcd7bb25ffcdae969b8ed75
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-06-18 00:11:55 +0100

    fix: fix task list component

--- task-management/pages/tasks/Tasks.tsx
+++ task-management/pages/tasks/Tasks.tsx
@@ -1,23 +1,30 @@
-import { TasksProvider, useTasks } from "@/app/contexts/TasksContext";
+import { TasksProvider, useTasks } from '@/app/contexts/TasksContext'
+import Loading from '../Loading'
 
 export default function Tasks() {
-    let state = useTasks();
-    return (
-        <>
-            <div className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">Task List</h1>
-                <ul className="bg-white divide-y divide-gray-300">
-                    {state.tasks.map(task => {
-                        return (
-                            <li key={task.id} className="flex items-center p-4">
-                                <input type="checkbox" className="mr-2" />
-                                <span className="flex-grow text-black">{task.id} - {task.name}</span>
-                                <button className="text-red-500 hover:text-red-600 focus:outline-none focus:ring focus:border-blue-300">Delete</button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </>
-    )
+  const { tasks, isLoading } = useTasks()
+  return (
+    <>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Task List</h1>
+        <ul className="bg-slate-950 divide-y divide-gray-900">
+          {isLoading && <Loading />}
+          {tasks &&
+            tasks.map((task: any) => {
+              return (
+                <li key={task.id} className="flex items-center p-4">
+                  <input type="checkbox" className="mr-2" />
+                  <span className="flex-grow text-white">
+                    {task.id} - {task.name}
+                  </span>
+                  <button className="text-red-500 hover:text-red-600 focus:outline-none focus:ring focus:border-blue-300">
+                    Delete
+                  </button>
+                </li>
+              )
+            })}
+        </ul>
+      </div>
+    </>
+  )
 }
--- task-management/pages/tasks/index.tsx
+++ task-management/pages/tasks/index.tsx
@@ -1,17 +1,15 @@
-import { TasksProvider } from '@/app/contexts/TasksContext';
-import Tasks from './Tasks';
+import { TasksProvider } from '@/app/contexts/TasksContext'
+import Tasks from './Tasks'
 import '../globals.css'
 
 const TaskPage = () => {
   return (
     <>
-      <TasksProvider>
-        <main className="min-h-screen mt-8">
-          <Tasks></Tasks>
-        </main>
-      </TasksProvider >
+      <main className="min-h-screen mt-8">
+        <Tasks></Tasks>
+      </main>
     </>
-  );
-};
+  )
+}
 
-export default TaskPage;
+export default TaskPage
