546c770118b0ac6cc757cbcb365c35f9896f8a1f
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-06-20 19:14:17 +0100

    add delete for the task

--- task-management/pages/api/tasks/[taskId].ts
+++ task-management/pages/api/tasks/[taskId].ts
@@ -1,8 +1,39 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next"
 
+const prisma = new PrismaClient({});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (!req.query.taskId) return res.status(404).send('Not Found');
     if (req.method === 'GET') {
-        return res.json( {'some task': 1});
+        const task = await prisma.task.findFirst({
+            where: {
+                id: parseInt(req.query.taskId as string)
+            }
+        });
+        return res.json({ task });
+    } else if (req.method === 'DELETE') {
+        const task = await prisma.task.delete({
+            where: {
+                id: parseInt(req.query.taskId as string)
+            }
+        });
+        return res.json({ task });
+    } else if (req.method === 'PUT') {
+        if (!req.body.name || !req.body.description)
+            return res.status(400).send('name or description is missing');
+
+        const task = await prisma.task.update({
+            where: {
+                id: parseInt(req.query.taskId as string)
+            },
+            data: {
+                name: req.body.name,
+                description: req.body.description
+            }
+        });
+
+        return res.json({ task });
     }
     return res.status(404);
-  }
+}
