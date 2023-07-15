9aea30055e509f2647ac32fcfab47ca7a1333d53
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-15 17:32:48 +0100

    core: add Generic button

--- task-management/app/ui/Button.tsx
+++ task-management/app/ui/Button.tsx
@@ -1,10 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
+
+
+
+interface IStyles {
+  primary: string;
+  small: string;
+  round: string;
+  secondary: string;
+}
 
 function Button({ children, disabled, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
-  const styles = {
+
+
+
+
+  const styles:IStyles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
     round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
@@ -14,9 +27,10 @@ function Button({ children, disabled, to, type, onClick }) {
 
   if (to)
     return (
-      <Link to={to} className={styles[type]}>
+      <Link href={to} className={styles[type]}>
         {children}
       </Link>
+
     );
 
   if (onClick)
