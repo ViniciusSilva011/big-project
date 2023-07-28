55ae37245cf9838396ba47d22b055c6b78982dd4
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-07-28 22:12:40 +0100

    auth logic for the authorized user

--- task-management/middleware.ts
+++ task-management/middleware.ts
@@ -4,19 +4,19 @@ import { getServerSession } from "next-auth";
 
 
 export default withAuth(
-    // `withAuth` augments your `Request` with the user's token.
-    function middleware(req,res) {
-      console.log('req.nextauth.token: ', req.nextauth.token)
-    },
-    {
-      callbacks: {
-         authorized: async (s) => {
-            console.log('res :>> ', s.req.cookies.get('next-auth.session-token'));
-            return true;
-        },
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req, res) {
+    console.log('req.nextauth.token: ', req.nextauth.token)
+  },
+  {
+    callbacks: {
+      authorized: async (s) => {
+        console.log('res :>> ', s.req.cookies.get('next-auth.session-token'));
+        return !!s.req.cookies.get('next-auth.session-token');
       },
-    }
-  )
+    },
+  }
+)
 export const config = {
-    matcher: '/api/:path*',
+  matcher: '/api/:path*',
 }
