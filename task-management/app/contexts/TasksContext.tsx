359d935da634afb1c195556092067700d107f6fa
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-12-02 17:09:02 +0000

    fix typescript errors

--- task-management/app/contexts/TasksContext.tsx
+++ task-management/app/contexts/TasksContext.tsx
@@ -15,61 +15,28 @@ interface Task {
 interface State {
   tasks: Task[]
   isLoading: boolean
-  currentTask: Task | null
   error: string
 }
 
+interface Action {
+  type: 'loading' | 'tasks/loaded' | 'tasks/deleted' | 'tasks/created' | 'rejected',
+  payload?: Task[] | number | string
+}
+
 const initialState = {
-  tasks: [],
+  tasks: [] as Task[],
   isLoading: false,
-  currentTask: {},
-  error: ''
+  error: '',
+  deleteTask: (a: any) => { },
+  createTask: (a: any) => { },
+  fetchTasks: (a: any) => { }
 }
 
-const TasksContext = createContext();
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'loading':
-      return { ...state, isLoading: true }
-
-    case 'tasks/loaded':
-      return { ...state, isLoading: false, tasks: action.payload }
-
-    // TODO: provisorio ate usar callback
-    case 'tasks/deleted':
-      return {
-        ...state,
-        isLoading: false,
-        tasks: state.tasks.filter(task => task.id !== action.payload)
-      }
-
-    case "city/created":
-      return {
-        ...state,
-        isLoading: false,
-        cities: [...state.tasks, action.payload],
-        currentCity: action.payload,
-      };
-
-
-    case 'rejected':
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload!
-      }
-
-    default:
-      throw new Error('Unknown action type')
-  }
-}
+const TasksContext = createContext(initialState);
 
 function TasksProvider({ children }: { children: React.ReactNode }) {
-  const [{ tasks, isLoading, currentTask, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  )
+  const [{ tasks, isLoading, error }, dispatch] = useReducer(reducer, initialState)
+
   async function fetchTasks() {
     dispatch({ type: 'loading' })
 
@@ -85,7 +52,6 @@ function TasksProvider({ children }: { children: React.ReactNode }) {
     }
   }
   useEffect(() => {
-
     fetchTasks()
   }, [])
 
@@ -108,7 +74,7 @@ function TasksProvider({ children }: { children: React.ReactNode }) {
 
   }
 
-  async function createTask(newTask) {
+  async function createTask(newTask: Task) {
     dispatch({ type: "loading" });
 
     try {
@@ -121,7 +87,7 @@ function TasksProvider({ children }: { children: React.ReactNode }) {
       });
       const data = await res.json();
 
-      dispatch({ type: "task/created", payload: data });
+      dispatch({ type: "tasks/created", payload: data });
     } catch {
       dispatch({
         type: "rejected",
@@ -131,31 +97,68 @@ function TasksProvider({ children }: { children: React.ReactNode }) {
   }
 
 
+  function reducer(state: State, action: Action) {
+    switch (action.type) {
+      case 'loading':
+        return { ...state, isLoading: true }
+
+      case 'tasks/loaded':
+        return { ...state, isLoading: false, tasks: action.payload as Task[] }
+
+      // TODO: provisorio ate usar callback
+      case 'tasks/deleted':
+        return {
+          ...state,
+          isLoading: false,
+          tasks: state.tasks.filter(task => task.id !== action.payload as number)
+        }
+
+      case "tasks/created":
+        return {
+          ...state,
+          isLoading: false,
+          cities: [...state.tasks, action.payload],
+          currentCity: action.payload,
+        };
+
+
+      case 'rejected':
+        return {
+          ...state,
+          isLoading: false,
+          error: action.payload as string
+        }
+
+      default:
+        throw new Error('Unknown action type')
+    }
+  }
 
   const value = useMemo(() => {
     return {
       tasks,
       isLoading,
-      currentTask,
       error,
       deleteTask,
       createTask,
       fetchTasks
     }
-  }, [tasks, isLoading, currentTask, error])
+  }, [tasks, isLoading, error])
 
   return <TasksContext.Provider value={value}>{children}</TasksContext.Provider>
 }
 
 type Context = {
-  fetchTasks: () => Promise<void>,
-  deleteTask: (id: string) => Promise<void>,
+  tasks: Task[],
   isLoading: boolean,
-  tasks: Task[]
+  error: string,
+  deleteTask: (id: string) => Promise<void>,
+  createTask: (task: Task) => Promise<void>,
+  fetchTasks: () => Promise<void>,
 }
 
 function useTasks() {
-  const context = useContext(TasksContext) as Context
+  const context = useContext(TasksContext) as unknown as Context
   if (context === undefined)
     throw new Error('TasksContext was used outside ok Tasks Provider')
   return context
