db2555ccd9a8ddfe5179141251cee7640b0f9bf2
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-12-03 16:16:18 +0000

    added min-h-screen to the root layout

--- task-management/pages/layout.tsx
+++ task-management/pages/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
     return (
         <>
             <Header />
-            <div>
+            <div className="min-h-screen">
                 {children}
             </div>
         </>
