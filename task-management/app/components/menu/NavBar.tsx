d842a765fa4ed056b6a9beef459aaecff58d7dbe
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-12-03 15:21:52 +0000

    add the profile component with the headless ui menu

--- task-management/app/components/menu/NavBar.tsx
+++ task-management/app/components/menu/NavBar.tsx
@@ -1,9 +1,9 @@
-import { signIn, signOut } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect } from 'react'
 import InsertTaskButton from '../tasks/InsertTaskButton'
+import ProfileMenu from './ProfileMenu'
 
 const NavBar = ({ user = '' }) => {
   const pathName = usePathname() as string;
@@ -96,61 +96,7 @@ const NavBar = ({ user = '' }) => {
                     />
                   </svg>
                 </button>
-
-                <div className="relative ml-3">
-                  <div>
-                    <button
-                      type="button"
-                      className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      id="user-menu-button"
-                      aria-expanded="false"
-                      aria-haspopup="true"
-                    >
-                      <span className="mr-2">{user}</span>
-                      <span className="sr-only">Open user menu</span>
-                      <Image
-                        width={80}
-                        height={80}
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </button>
-                  </div>
-
-                  <div
-                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="user-menu-button"
-                  >
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-0"
-                    >
-                      Your Profile
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-1"
-                    >
-                      Settings
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-2"
-                      onClick={() => signOut()}
-                    >
-                      Sign Out
-                    </a>
-                  </div>
-                </div>
+                <ProfileMenu />
               </div>
             </div>
             <div className="-mr-2 flex md:hidden">
@@ -229,65 +175,6 @@ const NavBar = ({ user = '' }) => {
             >
               Reports
             </a>
-          </div>
-          <div className="border-t border-gray-700 pb-3 pt-4">
-            <div className="flex items-center px-5">
-              <div className="flex-shrink-0">
-                <Image
-                  width={100}
-                  height={100}
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium leading-none text-white">
-                  {user}
-                </div>
-                <div className="text-sm font-medium leading-none text-gray-400"></div>
-              </div>
-              <button
-                type="button"
-                className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="mt-3 space-y-1 px-2">
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Settings
-              </a>
-              <button
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                onClick={() => signOut()}
-              >
-                signOut
-              </button>
-            </div>
           </div>
         </div>
       </nav>
---
+++ task-management/app/components/menu/ProfileMenu.tsx
@@ -0,0 +1,70 @@
+import { Menu } from '@headlessui/react';
+import { signOut } from 'next-auth/react'
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function ProfileMenu() {
+    const { data: session } = useSession();
+    const user = session?.user?.name ?? '';
+    return (
+        <div className="relative ml-3">
+
+            <Menu>
+                <Menu.Button
+                    type="button"
+                    className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true">
+
+                    <span className="mr-2">{user}</span>
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                        width={80}
+                        height={80}
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                    />
+                </Menu.Button>
+                <Menu.Items
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button">
+                    <Menu.Item>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700"
+                            role="menuitem"
+                            id="user-menu-item-0"
+                        >
+                            Your Profile
+                        </a>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700"
+                            role="menuitem"
+                            id="user-menu-item-1"
+                        >
+                            Settings
+                        </a>
+                    </Menu.Item>
+                    <Menu.Item >
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700"
+                            role="menuitem"
+                            id="user-menu-item-2"
+                            onClick={() => signOut()}
+                        >
+                            Sign Out
+                        </a>
+                    </Menu.Item>
+                </Menu.Items>
+            </Menu>
+
+        </div>
+    )
+}
