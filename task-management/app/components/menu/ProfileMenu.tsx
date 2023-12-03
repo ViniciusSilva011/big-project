f198018fa23c5d35a2f61e021443e122671074b1
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2023-12-03 15:25:34 +0000

    add the user image to the profile

--- task-management/app/components/menu/ProfileMenu.tsx
+++ task-management/app/components/menu/ProfileMenu.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 export default function ProfileMenu() {
     const { data: session } = useSession();
     const user = session?.user?.name ?? '';
+    //TODO: Fix the type later
+    const image = (session?.user as unknown as { image: string }).image;
     return (
         <div className="relative ml-3">
 
@@ -23,7 +25,7 @@ export default function ProfileMenu() {
                         width={80}
                         height={80}
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={image}
                         alt=""
                     />
                 </Menu.Button>
